79f90ad215f1a97842cb9943e76f80b6609a27c0
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-02-05 16:04:18 +0100

    added experiment of the n queens problem

---
+++ test/experiments/queens.pl
@@ -0,0 +1,23 @@
+/**
+  * 
+  * FILENAME: test/experiments/queens.pl
+  * DESCRIPTION: This file performs statistics with the problem of n queens.
+  * AUTHORS: José Antonio Riaza Valverde
+  * UPDATED: 05.02.2020
+  * 
+  **/
+
+:- use_module('../../src/semantics').
+:- use_module('../../src/environment').
+
+
+
+test_queens(X, Runtime, Inferences) :-
+    lattice_consult('../../sample/lat/bool.lat.pl'),
+    program_consult('../../sample/program/queens.fpl'),
+    statistics(runtime, [_,_]),
+    statistics(inferences, I0),
+    once(query(term('queens', [num(X)]), State)),
+    statistics(inferences, If),
+    statistics(runtime, [_,Runtime]),
+    Inferences is If-I0.
